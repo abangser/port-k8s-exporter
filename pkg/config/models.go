3e40cadadf8e2136df975af711e47ecf88dc340d
Abby Bangser <abby@paintedwavelimited.com>
2025-01-17 12:12:58 +0000

    feat: expose 'OverwriteCRDsActions' as a env var
    This will people to run the controller without overwriting any action updates they do in the UI.
    
    Ideally this will be exposed as well via a values file update in the helm chart.

--- pkg/config/models.go
+++ pkg/config/models.go
@@ -21,6 +21,7 @@ type ApplicationConfiguration struct {
 	PortClientSecret                string
 	EventListenerType               string
 	CreateDefaultResources          bool
+	OverwriteCRDsActions            bool
 	OverwriteConfigurationOnRestart bool
 	// These Configurations are used only for setting up the Integration on installation or when using OverwriteConfigurationOnRestart flag.
 	Resources                    []port.Resource
